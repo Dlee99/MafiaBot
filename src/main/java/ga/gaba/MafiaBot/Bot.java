d7831944fb4d64094b9d489215eb12c976945051
Gabe Lyczak <gaba@gaba.space>
2017-10-14 13:12:48 -0400

    Added main class

---
+++ src/main/java/ga/gaba/MafiaBot/Bot.java
@@ -0,0 +1,19 @@
+package ga.gaba.MafiaBot;
+
+import net.dv8tion.jda.core.AccountType;
+import net.dv8tion.jda.core.JDA;
+import net.dv8tion.jda.core.JDABuilder;
+
+/**
+ * Created by glyczak on 10/14/17.
+ */
+public class Bot {
+    private static final String token = "TOKEN_GOES_HERE";
+    public static JDA api;
+
+    public static void main(String[] args) throws Exception
+    {
+        api = new JDABuilder(AccountType.BOT).setToken(token).buildAsync();
+        api.addEventListener(new GameListner());
+    }
+}
